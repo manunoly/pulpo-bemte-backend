8271ac9fe04e15493c03c96b4c0e3a8d51e68d55
Manuel Almaguer <manunoly@Manuels-MacBook-Pro.local>
2019-11-23 08:00:14 -0500

    Agregando nombre de los estados para mapear

---
+++ app/Estado.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Estado extends Model
+{
+    protected $table = 'estados';
+    protected $primaryKey = 'estado'; 
+    public $incrementing = false;
+    
+    protected $fillable = [
+        'estado', 'nombre',
+    ];
+}
